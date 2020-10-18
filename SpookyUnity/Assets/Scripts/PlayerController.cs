fe55e1d48810d94088930b801a71dc133d674e9d
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2020-10-18 19:16:37 +0100

    Added character and animations

---
+++ SpookyUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed *Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            animator.SetBool("Walking", true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            animator.SetBool("Walking", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            // transform.Translate(-Vector3.right * speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            animator.SetBool("Walking", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0,90, 0);
+            //transform.Translate(Vector3.right * speed * Time.deltaTime);
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+}
