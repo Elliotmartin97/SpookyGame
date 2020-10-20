e974bf846c537eb785fbf9316f73bba808376b17
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2020-10-20 22:29:08 +0100

    Working on Torch Light Fade Down over time (WIP)

---
+++ SpookyUnity/Assets/Scripts/LightFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFade : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.Experimental.Rendering.Universal.Light2D Light2D;
+
+    private float minLuminosity = 0;
+    private float maxLuminosity = 1;
+
+    float counter = 0f;
+
+    void Start()
+    {
+        maxLuminosity = 1;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
